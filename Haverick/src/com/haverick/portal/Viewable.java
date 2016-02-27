a9fe4aadaf3ba2beda73c66d360ef59aa995765d
Colton Franco <coltonf93@gmail.com>
2016-02-27 17:39:53 -0600

    Updated css for placeholders

--- Haverick/src/com/haverick/portal/Viewable.java
+++ Haverick/src/com/haverick/portal/Viewable.java
@@ -7,9 +7,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.haverick.util.Channel;
-import com.haverick.util.RoviCtrl;
+
 
 @WebServlet("/Viewable")
 public class Viewable extends HttpServlet {
